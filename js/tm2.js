2fa4e11ed32e74cfde8128e7eced63448f538582
田中一彦 <kzhk.tnk@gmail.com>
2019-12-09 20:06:00 +0900

    gtm

--- js/tm2.js
+++ js/tm2.js
@@ -1,5 +1,3 @@
-<!-- Google Tag Manager (noscript) -->
 <noscript>
     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5V8JSNJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
-<!-- End Google Tag Manager (noscript) -->
